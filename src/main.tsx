919333b66127ad08aa777f56506e64cc1b22473d
Thanh Nguyen <moringuyenvietnam@gmail.com>
2026-01-31 20:36:48 +0900

    chore: Remove debug overlay and logs - App is working

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-console.log("Mounting React App...");
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-    console.error("Root element not found!");
-} else {
-    console.log("Root element found:", rootElement);
-    createRoot(rootElement).render(<App />);
-}
+createRoot(document.getElementById("root")!).render(<App />);
